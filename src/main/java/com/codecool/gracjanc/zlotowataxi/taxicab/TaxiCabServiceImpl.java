f0f8ab445c4efd1a3e49c74ff3b88af4cfcc40e6
Gracjan Cisakowski <gracjan.cisakowski@gmail.com>
2018-01-19 00:14:05 +0100

    Implement TaxiCabService class

--- src/main/java/com/codecool/gracjanc/zlotowataxi/taxicab/TaxiCabServiceImpl.java
+++ src/main/java/com/codecool/gracjanc/zlotowataxi/taxicab/TaxiCabServiceImpl.java
@@ -1,23 +1,43 @@
 package com.codecool.gracjanc.zlotowataxi.taxicab;
 
+import com.codecool.gracjanc.zlotowataxi.exception.TaxiCabNotFoundException;
+
 public class TaxiCabServiceImpl implements TaxiCabService {
-    @Override
-    public Iterable<TaxiCab> findAll() {
-        return null;
+    private TaxiCabRepository taxiCabRepository;
+
+    public TaxiCabServiceImpl(TaxiCabRepository taxiCabRepository) {
+        this.taxiCabRepository = taxiCabRepository;
     }
 
     @Override
-    public TaxiCab findOne(Integer id) {
-        return null;
+    public Iterable<TaxiCab> findAll() {
+        return this.taxiCabRepository.findAll();
+    }
+
+    @Override
+    public TaxiCab findOne(Integer id) throws TaxiCabNotFoundException{
+        TaxiCab taxiCab = this.taxiCabRepository.findOne(id);
+
+        if (taxiCab == null) {
+            throw new TaxiCabNotFoundException(id);
+        }
+
+        return taxiCab;
     }
 
     @Override
     public void save(TaxiCab driver) {
-
+        this.taxiCabRepository.save(driver);
     }
 
     @Override
     public void delete(Integer id) {
+        TaxiCab taxiCab = this.taxiCabRepository.findOne(id);
 
+        if (taxiCab == null) {
+            throw new TaxiCabNotFoundException(id);
+        }
+
+        this.taxiCabRepository.delete(id);
     }
 }
